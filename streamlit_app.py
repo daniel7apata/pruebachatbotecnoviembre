539827484d1d93dcd085900fdf0e9d25783d67ce
Daniel Zapata <90068737+daniel7apata@users.noreply.github.com>
2024-11-23 04:19:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,37 +9,38 @@ st.title("Web Scrapping+Chatbot")
 base_url = st.text_input("Link")
 
 if not base_url:
-  st.write("Ingrese link")
+	st.write("Ingrese link")
 else:
-  def fetch_page(base_url):
-      response = requests.get(base_url)
-      if response.status_code == 200:
-          return BeautifulSoup(response.content, 'html.parser')
-      else:
-          print(f"Failed to retrieve {url}")
-          return None
-  
-  def extract_titles(soup):
-      titles = ""
-      if soup:
-          product_titles = soup.find_all()
-          for title in product_titles:
-              titles += title.get_text(strip=True) + "      "
-      return titles
-  
-  num_pages = 1
-  
-  all_titles = []
-  
-  for page in range(1, num_pages + 1):
-      if page == 1:
-          url = base_url
-      else:
-          url = f"{base_url}?page={page}"
-      st.write(f"Fetching page {page}: {url}")
-      soup = fetch_page(url)
-      titles = extract_titles(soup)
-      st.write(titles)
+	def fetch_page(base_url):
+		response = requests.get(base_url)
+		if response.status_code == 200:
+			return BeautifulSoup(response.content, 'html.parser')
+		else:
+			print(f"Failed to retrieve {base_url}")
+			return None
+	
+	def extract_titles(soup):
+		titles = ""
+		if soup:
+			product_titles = soup.find_all()
+			for title in product_titles:
+				titles += title.get_text(strip=True) + "      "
+		return titles
+	
+	num_pages = 1
+	
+	all_titles = []
+	
+	for page in range(1, num_pages + 1):
+		if page == 1:
+			url = base_url
+		else:
+			url = f"{base_url}?page={page}"
+		st.write(f"Fetching page {page}: {url}")
+		soup = fetch_page(url)
+		titles = extract_titles(soup)
+		st.write(titles)
+
 
     
     indicacion = " finaliza tu respuesta diciendo 'segun Sammy'"
@@ -55,7 +56,7 @@ else:
     openai_api_key = st.text_input("OpenAI API Key", type="password")
     if not openai_api_key:
         st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-    else:
+  else:
     
         # Create an OpenAI client.
         client = OpenAI(api_key=openai_api_key)
